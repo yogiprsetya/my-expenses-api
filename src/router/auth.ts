c50bb99ace5784902137c5778f0601e482d60ff5
My <hello@yogiprasetya.com>
2023-12-05 20:37:59 +0700

    remove router dir

--- src/router/auth.ts
+++
@@ -1,8 +0,0 @@
-import { signupWithGoogle } from 'controller/auth'
-import express from 'express'
-
-const router = express.Router()
-
-router.post('/signup', signupWithGoogle)
-
-export { router as authRoutes }
